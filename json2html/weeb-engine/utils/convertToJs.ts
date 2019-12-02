631c7e5578f896ecf55d8f5105fe76d4a7e51902
Gurimarukin <naxel@laposte.net>
2019-12-02 16:17:55 +0100

    Fix convertToJs

--- json2html/weeb-engine/utils/convertToJs.ts
+++ json2html/weeb-engine/utils/convertToJs.ts
@@ -1,26 +1,12 @@
-import * as O from 'fp-ts/lib/Option'
-import { pipe } from 'fp-ts/lib/pipeable'
+import falafel from 'falafel'
 
 const convertToJs = (code: string): string =>
-    pipe(
-        O.fromNullable(` ${code} `.match(word)),
-        O.map(_ =>
-            _.reduce((acc, m) => {
-                const trimedM = m.trim()
-                if (kwords.indexOf(trimedM) === -1) {
-                    return acc.replace(trimedM, `window._${trimedM}`)
-                }
-                return acc
-            }, code).replace('==', '===')
-        ),
-        O.getOrElse(() => code)
-    )
+    falafel(code.replace('==', '==='), node => {
+        if (
+            node.type === 'Identifier' &&
+            (node.parent.object === undefined || node.parent.object === node)
+        ) {
+            node.update(`window._${node.name}`)
+        }
+    }).toString()
 export default convertToJs
-
-const word = /\W([a-zA-Z_]\w*)\W/g
-const kwords = [
-    // 'break', 'case', 'catch', 'class', 'const', 'continue', 'debugger', 'default', 'delete', 'do', 'else', 'export', 'extends', 'finally', 'for', 'function', 'if', 'import', 'in', 'instanceof', 'new', 'return', 'super', 'switch', 'this', 'throw', 'try', 'typeof', 'var', 'void', 'while', 'with', 'yield',
-    // 'null', 'undefined',
-    'true',
-    'false'
-]
